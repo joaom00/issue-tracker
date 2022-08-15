d66d5a25f7c8bd2902a9db387f4b71c32d5c937c
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2022-08-14 21:11:05 -0300

    feat: filtering status

---
+++ src/components/StatusSelect.jsx
@@ -0,0 +1,20 @@
+const possibleStatus = [
+  { id: "backlog", label: "Backlog" },
+  { id: "todo", label: "To-do" },
+  { id: "inProgress", label: "In Progress" },
+  { id: "done", label: "Done" },
+  { id: "cancelled", label: "Cancelled" },
+];
+
+export function StatusSelect({ value, onChange }) {
+  return (
+    <select value={value} onChange={onChange} className="status-select">
+      <option value="">Select a status to filter</option>
+      {possibleStatus.map((status) => (
+        <option key={status.id} value={status.id}>
+          {status.label}
+        </option>
+      ))}
+    </select>
+  );
+}
--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
+import { StatusSelect } from "../components/StatusSelect";
 
 export default function Issues() {
   const [labels, setLabels] = React.useState([]);
+  const [status, setStatus] = React.useState("");
   return (
     <div>
       <main>
         <section>
           <h1>Issues</h1>
-          <IssuesList labels={labels} />
+          <IssuesList labels={labels} status={status} />
         </section>
         <aside>
           <LabelList
@@ -23,6 +25,11 @@ export default function Issues() {
                   : currentLabels.concat(label)
               )
             }
+          />
+          <h3>Status</h3>
+          <StatusSelect
+            value={status}
+            onChange={(event) => setStatus(event.target.value)}
           />
         </aside>
       </main>
